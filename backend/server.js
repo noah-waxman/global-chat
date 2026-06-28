0f9c4d962784cb7adeff8bb6cb6bfb87f5f50e2e
Noah Waxman <noah@Noahs-Mac-mini.local>
2026-06-28 00:34:38 -0700

    Modified database initialization and environment variable integration

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASS,
+  DB_NAME
+} = process.env;
+
+const pgp = require('pg-promise')();
+const db = pgp(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}`);
+
 const express = require('express');
 const app = express();
 const port = 3000;
