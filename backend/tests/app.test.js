a49da39a65b5194432453ebbe31863419eab4fe0
Noah Waxman <noah@Noahs-Mac-mini.local>
2026-07-11 00:23:22 -0700

    Added login tests

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -6,6 +6,10 @@ afterAll(async () => {
   await db.pgp.end();
 });
 
+beforeEach(async () => {
+  await db.none("TRUNCATE TABLE users, session RESTART IDENTITY CASCADE");
+});
+
 describe("POST /auth/register", () => {
   describe("given a username, display name, and password", () => {
     test("should respond with a 201 status code", async () => {
@@ -17,6 +21,55 @@ describe("POST /auth/register", () => {
           password: "password",
         })
         .expect(201);
+
+      const cookies = response.headers["set-cookie"];
+      expect(cookies).toBeDefined();
+
+      const sessionCookie = cookies.find((cookie) =>
+        cookie.startsWith("connect.sid="),
+      );
+
+      expect(sessionCookie).toBeDefined();
+    });
+  });
+});
+
+describe("POST /auth/login", () => {
+  describe("given an email and password", () => {
+    test("should respond with a 201 status code", async () => {
+      await request(app)
+        .post("/auth/register")
+        .send({
+          displayName: "test",
+          email: "test@example.com",
+          password: "password",
+        })
+        .expect(201);
+
+      const response = await request(app).post("/auth/login").send({
+        email: "test@example.com",
+        password: "password",
+      });
+
+      const userId = response.body["id"];
+
+      const email = response.body["email"];
+
+      const displayName = response.body["displayName"];
+
+      const cookies = response.headers["set-cookie"];
+
+      const sessionCookie = cookies.find((cookie) =>
+        cookie.startsWith("connect.sid="),
+      );
+
+      expect(userId).toBeDefined();
+      expect(email).toBe("test@example.com");
+      expect(displayName).toBe("test");
+      expect(cookies).toBeDefined();
+      expect(sessionCookie).toBeDefined();
+
+      console.log(response.headers);
     });
   });
 });
