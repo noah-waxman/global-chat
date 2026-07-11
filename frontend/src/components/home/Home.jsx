9acc181d6834e61b26e0f54144a024a004764ddc
Noah Waxman <noah@Noahs-Mac-mini.local>
2026-07-11 15:31:36 -0700

    Modified home page to look like chat

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -9,5 +9,17 @@ export default function Home() {
 		navigate('/login');
 	}
 
-	return <p className="text-2xl text-white">Home</p>;
+	return (
+		<div className="h-screen flex">
+			<div className="h-full w-90 bg-surface-a20"></div>
+
+			<div className="mt-auto pb-10 pt-10 pl-6 pr-6 bg-surface-a10 w-full flex items-center gap-4">
+				<input className="flex-1 bg-surface-a20 placeholder:text-white/60 text-white text-2xl rounded-xl px-3 py-5 border border-transparent focus:outline-none focus:border-white" />
+
+				<button className="size-18 flex items-center justify-center text-white hover:cursor-pointer">
+					<span className="material-symbols-rounded !text-5xl">send</span>
+				</button>
+			</div>
+		</div>
+	);
 }
